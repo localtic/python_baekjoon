48c6d0acd105a2f33e8c7f3388c2fb26163275ca
localtic <gussm150@gmail.com>
2022-02-08 22:21:59 +0900

    python string

---
+++ lv7_string.py
@@ -0,0 +1,13 @@
+# ord("문자") 문자를 아스키 코드로 반환
+# chr(num) 숫자를 아스키 코드로 반환
+# 아스키코드 출력할 때 숫자인지 아닌지 판별 안 해줘도 됨
+
+#11654
+N = input()
+print(ord(N))
+
+#11720
+N = int(input())
+A = [int(i) for i in input()]
+print(sum(A))
+
