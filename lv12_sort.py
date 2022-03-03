c5828cc1b017e2a1fddffdd5d7a67d04c737a0b9
localtic <gussm150@gmail.com>
2022-03-03 22:54:22 +0900

    python sort

--- lv12_sort.py
+++ lv12_sort.py
@@ -9,3 +9,16 @@ numbers.sort()
 
 for i in numbers:
     print(i)
+
+# 2751
+import sys
+N = int(input())
+
+numbers = []
+for i in range(N):
+    numbers.append(int(sys.stdin.readline()))
+
+numbers.sort()
+
+for i in numbers:
+    print(i)
